49bc2b70bea7e7d8ff43be2a0dfd6cafca1c1f79
Moriyuki HIRATA <moriyuki@gmail.com>
2013-06-20 16:39:49 +0900

    カレンダー情報を取得した

---
+++ app/assets/javascripts/members.js
@@ -0,0 +1,5 @@
+$('a.show').click(function(){
+  $.get("member_show_path"), function(data) {
+    alert('return');
+  }
+})
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Hackathon::Application.routes.draw do
+  get 'members/index'
+  get 'members/show'
   get 'oauth2authorize' => 'auth#oauth2authorize'
   get 'oauth2callback'  => 'auth#oauth2callback'
   get 'auth/result'
