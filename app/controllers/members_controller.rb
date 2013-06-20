fbee5f6b7637ad748b472ae9955b1d9167053e2c
kbaba1001 <kbaba1001@gmail.com>
2013-06-20 17:57:07 +0900

    show time

--- app/controllers/members_controller.rb
+++ app/controllers/members_controller.rb
@@ -1,12 +1,13 @@
 class MembersController < ApplicationController
   def index
   end
+
   def show
     calendar = @client.discovered_api('calendar', 'v3')
 
     events = @client.execute(
-      :api_method => calendar.events.list,
-      :parameters => {'calendarId' => "m-hirata@esm.co.jp", 'timeMax' => '2013-06-19T00:00:00Z', 'timeMin' => '2013-06-18T00:00:00Z'}
+      api_method: calendar.events.list,
+      :parameters => {'calendarId' => "m-hirata@esm.co.jp", 'timeMax' => '2013-06-20T00:00:00Z', 'timeMin' => '2013-06-21T00:00:00Z'}
     )
     @cal = events.data
   end
