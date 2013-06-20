70f85ec21f081880c08e0bb920de836597dcf49d
kbaba1001 <kbaba1001@gmail.com>
2013-06-21 01:43:22 +0900

    add init member datas

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,2 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+Member.create(name: "平田守幸", email: "m-hirata@esm.co.jp", at_home: false)
+Member.create(name: "馬場一樹", email: "k-baba@esm.co.jp", at_home: false)
